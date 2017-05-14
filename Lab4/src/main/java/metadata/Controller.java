59ebf90c9157bac500903288947e3318335e5e93
Andrew Vychev <andrey.v@inventain.com>
2017-05-14 23:10:55 +0300

    fix 4 lab

--- Lab4/src/main/java/metadata/Controller.java
+++ Lab4/src/main/java/metadata/Controller.java
@@ -6,12 +6,10 @@ import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 import javafx.application.Platform;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
@@ -21,21 +19,6 @@ public class Controller {
 
     @FXML
     private TableView<ImageMetadata> data;
-
-    @FXML
-    private TableColumn<ImageMetadata, String> nameCol;
-
-    @FXML
-    private TableColumn<ImageMetadata, String> dimensionCol;
-
-    @FXML
-    private TableColumn<ImageMetadata, String> resolutionCol;
-
-    @FXML
-    private TableColumn<ImageMetadata, String> compressionCol;
-
-    @FXML
-    private TableColumn<ImageMetadata, String> fileSizeCol;
 
     @FXML
     private void onQuitClicked() {
@@ -64,43 +47,67 @@ public class Controller {
 
     private void loadDirectories(String name, Metadata metadata) {
         ObservableList<ImageMetadata> items = data.getItems();
+        ImageMetadata item = new ImageMetadata();
+        item.setName(name);
+        StringBuilder other = new StringBuilder();
         for (Directory directory : metadata.getDirectories()) {
-            ImageMetadata item = new ImageMetadata();
-            item.setName(name);
             for (Tag tag : directory.getTags()) {
+                System.out.println(tag.getTagName() + " " + tag.getDescription());
                 if ("Compression Type".equals(tag.getTagName())) {
                     item.setCompression(tag.getDescription());
+                    continue;
                 }
 
                 if ("Image Height".equals(tag.getTagName())) {
                     item.setResolution(tag.getDescription().split(" ")[0]);
+                    continue;
                 }
 
-                if ("Image Height".equals(tag.getTagName())) {
+                if ("Image Width".equals(tag.getTagName())) {
                     item.setResolution(item.getResolution() + "x" + tag.getDescription().split(" ")[0]);
+                    continue;
                 }
 
                 if ("X Max".equals(tag.getTagName())) {
                     item.setResolution(tag.getDescription().split(" ")[0]);
+                    continue;
                 }
 
                 if ("Y Max".equals(tag.getTagName())) {
                     item.setResolution(item.getResolution() + "x" + tag.getDescription().split(" ")[0]);
+                    continue;
                 }
 
                 if ("Vertical DPI".equals(tag.getTagName())) {
                     item.setDimension(tag.getDescription());
+                    continue;
                 }
 
                 if ("Y Resolution".equals(tag.getTagName())) {
                     item.setDimension(tag.getDescription().split(" ")[0]);
+                    continue;
                 }
 
                 if ("File Size".equals(tag.getTagName())) {
                     item.setFileSize(tag.getDescription());
+                    continue;
                 }
+
+                if ("File Modified Date".equals(tag.getTagName())) {
+                    item.setFileModifiedDate(tag.getDescription());
+                    continue;
+                }
+
+                if ("Orientation".equals(tag.getTagName())) {
+                    item.setOrientation(tag.getDescription());
+                    continue;
+                }
+
+                other.append(tag.getTagName()).append(" : ").append(tag.getDescription()).append('\n');
             }
-            items.add(item);
         }
+
+        item.setOther(other.toString());
+        items.add(item);
     }
 }
--- Lab4/src/main/java/metadata/ImageMetadata.java
+++ Lab4/src/main/java/metadata/ImageMetadata.java
@@ -12,26 +12,38 @@ public class ImageMetadata {
     private SimpleStringProperty resolution;
     private SimpleStringProperty compression;
     private SimpleStringProperty fileSize;
+    private SimpleStringProperty fileModifiedDate;
+    private SimpleStringProperty orientation;
+    private SimpleStringProperty other;
 
     public ImageMetadata(String name,
                          String dimension,
                          String resolution,
                          String compression,
-                         String fileSize) {
+                         String fileSize,
+                         String fileModifiedDate,
+                         String orientation,
+                         String other) {
         this();
+        this.fileModifiedDate.set(fileModifiedDate);
+        this.orientation.set(orientation);
         this.name.set(name);
         this.dimension.set(dimension);
         this.resolution.set(resolution);
         this.compression.set(compression);
         this.fileSize.set(fileSize);
+        this.other.set(other);
     }
 
     public ImageMetadata() {
+        orientation = new SimpleStringProperty("");
+        fileModifiedDate = new SimpleStringProperty("");
         name = new SimpleStringProperty("");
         dimension = new SimpleStringProperty("");
         resolution = new SimpleStringProperty("");
         compression = new SimpleStringProperty("");
         fileSize = new SimpleStringProperty("");
+        other = new SimpleStringProperty("");
     }
 
     public String getName() {
@@ -72,5 +84,29 @@ public class ImageMetadata {
 
     public String getFileSize() {
         return fileSize.get();
+    }
+
+    public String getFileModifiedDate() {
+        return fileModifiedDate.get();
+    }
+
+    public void setFileModifiedDate(String fileModifiedDate) {
+        this.fileModifiedDate.set(fileModifiedDate);
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation.set(orientation);
+    }
+
+    public String getOrientation() {
+        return orientation.get();
+    }
+
+    public String getOther() {
+        return other.get();
+    }
+
+    public void setOther(String other) {
+        this.other.set(other);
     }
 }
